6af9783a474907ed1f9047e2386af5037839e358
Elvio Galeano <elvio.galeano827@gmail.com>
2023-03-08 03:16:39 -0300

    all component in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import About from './components/About'
+import Certificate from './components/Certificate'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
+import Tecnology from './components/Tecnology'
+import Works from './components/Works'
+
+
+
+function App() {
+
+  return (
+    < >
+      <Header />
+      <Home />
+      <About />
+      <Certificate />
+      <Tecnology />
+      <Works />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
