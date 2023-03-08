25a4af6034238d901bc7f311e1ce008f56c13e3b
Elvio Galeano <elvio.galeano827@gmail.com>
2023-03-08 03:57:33 -0300

    fix button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
   return (
     <footer className='flex w-full justify-around text-4xl mb-5 flex-wrap gap-3'>
         <div className='item-start flex'>
-            <a href='Elvio_Galeano.pdf'  target="_blank" rel="noopener noreferrer" download="Elvio_Galeano.pdf" className='text-2xl font-bold font-serif duration-200 hover:text-orange-400' >Descargar CV</a>
+            <a href='Elvio_Galeano.pdf'  target="_blank" rel="noopener noreferrer" download="Elvio_Galeano.pdf" className='text-2xl font-bold font-serif duration-200 hover:text-orange-400' >Download CV</a>
         </div>
         <div className='flex gap-3 '>
             <a target='_blank' className='hover:animate-bounce duration-200 hover:text-blue-600' href='https://www.linkedin.com/in/elviogaleano-fullstack-developer/'  ><AiFillLinkedin /></a>
