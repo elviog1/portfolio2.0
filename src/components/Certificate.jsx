96fcaa2f8ed7f5372f42e95fc60b84324e84b06c
Elvio Galeano <elvio.galeano827@gmail.com>
2023-04-07 13:44:10 -0300

    add certificate sql

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -20,9 +20,18 @@ export default function Certificate() {
         </div>
         <div>
             <div className='flex justify-center flex-wrap gap-4'>
-                <img src='https://images.credly.com/size/340x340/images/9ab2bbf5-a5cd-4c36-9c5a-1ce7e0e0cd32/Insignia_MERN-20.png' alt='Certificado de ' className='w-60 shadow-2xl' />
-                <img src='openbootcampJS.jpeg' alt='Certificado' className='w-80 shadow-2xl' />
-                <img src='freecodecamp.jpeg' alt='Certificado' className='w-80 shadow-2xl' />
+                <a target='_blank' href='https://www.credly.com/badges/b95e81c0-105e-4baa-a0e3-93ec593ceb7a/linked_in_profile'>
+                    <img src='https://images.credly.com/size/340x340/images/9ab2bbf5-a5cd-4c36-9c5a-1ce7e0e0cd32/Insignia_MERN-20.png' alt='Certificado de Mindhub' className='w-60 shadow-2xl' />
+                </a>
+                <a target='_blank'>
+                    <img src='openbootcampJS.jpeg' alt='Certificado de JS' className='w-80 shadow-2xl' />
+                </a>
+                <a target='_blank' href='https://www.freecodecamp.org/certification/Elviog1/javascript-algorithms-and-data-structures'>
+                    <img src='freecodecamp.jpeg' alt='Certificado de JS' className='w-80 shadow-2xl' />
+                </a>
+                <a target='_blank' href='https://storage.googleapis.com/openvitae-prod/diplomas%2F2907393f-6c9a-4568-9e78-c9f3b06a47f9.pdf'>
+                    <img src='openbootcamp-sql.jpeg' alt='Certificado de SQL' className='w-80 shadow-2xl' />
+                </a>
             </div>
         </div>
     </section>
