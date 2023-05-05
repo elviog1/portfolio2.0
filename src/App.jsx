fb81db50afccb1a3676b13bfd3b992b0005c0853
Elvio Galeano <elvio.galeano827@gmail.com>
2023-05-04 21:08:32 -0300

    added Square components in background

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
+import Square from './components/Square'
+import Square1 from './components/Square1'
+import Square2 from './components/Square2'
 import Tecnology from './components/Tecnology'
 import Works from './components/Works'
 
@@ -13,7 +16,10 @@ import Works from './components/Works'
 function App() {
 
   return (
-    < >
+    <>
+      <Square col="bg-orange-400" width={"w-16"} height={"h-10"} />
+      <Square1  col="bg-red-500" width={"w-8"} height={"h-8"} />
+      <Square2 col="bg-emerald-300" width={"w-4"} height={"h-4"} />
       <Header />
       <Home />
       <About />
---
+++ src/components/Square.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+
+function Square1({col,width,height}) {
+  useEffect(() => {
+    const square = document.querySelector('.background-square');
+
+    function moveSquare() {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      const randomX = Math.floor(Math.random() * (windowWidth - 20));
+      const randomY = Math.floor(Math.random() * (windowHeight - 20));
+
+      const deltaX = randomX - parseInt(square.style.left || 0);
+      const deltaY = randomY - parseInt(square.style.top || 0);
+
+      const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+      const duration = distance * 6;
+
+      square.style.transition = `transform ${duration}ms linear`;
+      square.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+
+      setTimeout(() => {
+        square.style.transition = '';
+        moveSquare();
+      }, duration);
+    }
+
+    moveSquare();
+  }, []);
+
+  return <div className={`background-square rounded fixed ${width} ${height} ${col} -z-10`}></div>;
+}
+
+export default Square1;
