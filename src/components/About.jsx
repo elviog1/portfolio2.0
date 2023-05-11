d684257487cace3a3eb2bf02bbc109d15d4decc2
Elvio Galeano <elvio.galeano827@gmail.com>
2023-05-11 15:31:51 -0300

    added padding about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 export default function About() {
   const {t} = useTranslation()
   return (
-    <section id='about' className='flex justify-center items-center max-w-6xl mx-auto my-0 '>
+    <section id='about' className='flex justify-center items-center  px-4 '>
         <div className='w-full'>
             <div className='flex justify-evenly w-full flex-wrap '>
                 <img src='elvio-selfie.jpeg' alt='Picture Elvio' className='rounded-full w-64 h-64 object-cover shadow' />
